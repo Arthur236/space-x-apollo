6b54baf708ff87cce624346575fb008856608b78
Arthur Thungu <31339738+Arthur236@users.noreply.github.com>
2019-08-29 22:07:28 +0300

    Setup authorization

--- server/src/resolvers.js
+++ server/src/resolvers.js
@@ -6,11 +6,13 @@ module.exports = {
       const allLaunches = await dataSources.launchAPI.getAllLaunches();
       // we want these in reverse chronological order
       allLaunches.reverse();
+
       const launches = paginateResults({
         after,
         pageSize,
-        results: allLaunches
+        results: allLaunches,
       });
+
       return {
         launches,
         cursor: launches.length ? launches[launches.length - 1].cursor : null,
@@ -19,13 +21,56 @@ module.exports = {
         hasMore: launches.length
           ? launches[launches.length - 1].cursor !==
           allLaunches[allLaunches.length - 1].cursor
-          : false
+          : false,
       };
     },
     launch: (_, { id }, { dataSources }) =>
       dataSources.launchAPI.getLaunchById({ launchId: id }),
     me: async (_, __, { dataSources }) =>
       dataSources.userAPI.findOrCreateUser(),
+  },
+  Mutation: {
+    bookTrips: async (_, { launchIds }, { dataSources }) => {
+      const results = await dataSources.userAPI.bookTrips({ launchIds });
+      const launches = await dataSources.launchAPI.getLaunchesByIds({
+        launchIds,
+      });
+
+      return {
+        success: results && results.length === launchIds.length,
+        message:
+          results.length === launchIds.length
+            ? 'trips booked successfully'
+            : `the following launches couldn't be booked: ${launchIds.filter(
+            id => !results.includes(id),
+            )}`,
+        launches,
+      };
+    },
+    cancelTrip: async (_, { launchId }, { dataSources }) => {
+      const result = dataSources.userAPI.cancelTrip({ launchId });
+
+      if (!result)
+        return {
+          success: false,
+          message: 'failed to cancel trip',
+        };
+
+      const launch = await dataSources.launchAPI.getLaunchById({ launchId });
+      return {
+        success: true,
+        message: 'trip cancelled',
+        launches: [launch],
+      };
+    },
+    login: async (_, { email }, { dataSources }) => {
+      const user = await dataSources.userAPI.findOrCreateUser({ email });
+      if (user) return new Buffer(email).toString('base64');
+    },
+  },
+  Launch: {
+    isBooked: async (launch, _, { dataSources }) =>
+      dataSources.userAPI.isBookedOnLaunch({ launchId: launch.id }),
   },
   Mission: {
     // make sure the default size is 'large' in case user doesn't specify
@@ -34,10 +79,6 @@ module.exports = {
         ? mission.missionPatchSmall
         : mission.missionPatchLarge;
     },
-  },
-  Launch: {
-    isBooked: async (launch, _, { dataSources }) =>
-      dataSources.userAPI.isBookedOnLaunch({ launchId: launch.id }),
   },
   User: {
     trips: async (_, __, { dataSources }) => {
@@ -48,7 +89,7 @@ module.exports = {
 
       // look up those launches by their ids
       return (
-        await dataSources.launchAPI.getLaunchesByIds({
+        dataSources.launchAPI.getLaunchesByIds({
           launchIds,
         }) || []
       );
--- server/src/schema.js
+++ server/src/schema.js
@@ -2,7 +2,7 @@ const { gql } = require('apollo-server');
 
 const typeDefs = gql`
   type Query {
-    launches( # replace the current launches query with this one.
+    launches(
       """
       The number of results to show. Must be >= 1. Default = 20
       """
@@ -15,18 +15,28 @@ const typeDefs = gql`
     launch(id: ID!): Launch
     me: User
   }
-
+  type Mutation {
+    # if false, signup failed -- check errors
+    bookTrips(launchIds: [ID]!): TripUpdateResponse!
+    # if false, cancellation failed -- check errors
+    cancelTrip(launchId: ID!): TripUpdateResponse!
+    login(email: String): String # login token
+  }
+  type TripUpdateResponse {
+    success: Boolean!
+    message: String
+    launches: [Launch]
+  }
   """
   Simple wrapper around our list of launches that contains a cursor to the
   last item in the list. Pass this cursor to the launches query to fetch results
   after these.
   """
-  type LaunchConnection { # add this below the Query type as an additional type.
+  type LaunchConnection {
     cursor: String!
     hasMore: Boolean!
     launches: [Launch]!
   }
-
   type Launch {
     id: ID!
     site: String
@@ -34,44 +44,23 @@ const typeDefs = gql`
     rocket: Rocket
     isBooked: Boolean!
   }
-
   type Rocket {
     id: ID!
     name: String
     type: String
   }
-
   type User {
     id: ID!
     email: String!
     trips: [Launch]!
   }
-
   type Mission {
     name: String
-    missionPatch(mission: String, size: PatchSize): String
+    missionPatch(size: PatchSize): String
   }
-
   enum PatchSize {
     SMALL
     LARGE
   }
-
-  type Mutation {
-    # if false, booking trips failed -- check errors
-    bookTrips(launchIds: [ID]!): TripUpdateResponse!
-
-    # if false, cancellation failed -- check errors
-    cancelTrip(launchId: ID!): TripUpdateResponse!
-
-    login(email: String): String # login token
-  }
-
-  type TripUpdateResponse {
-    success: Boolean!
-    message: String
-    launches: [Launch]
-  }
 `;
-
 module.exports = typeDefs;
